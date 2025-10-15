1265032a229c5d6f132eb5bfe5292ecc98dfc126
David Morrison <dave@fluid.rip>
2025-10-14 22:50:39 -0400

    Additonal URL corrections

--- src/fmClient.js
+++ src/fmClient.js
@@ -14,17 +14,18 @@ export async function runFmScript(scriptParam) {
   const { FM_SERVER, FM_DB, FM_SCRIPT, FM_USER, FM_PASS } = process.env;
 
   const base = FM_SERVER?.replace(/\/$/, "");
-  const url =
-    `${base}/fmi/odata/v4/${encodeURIComponent(FM_DB)}.fmp12/${encodeURIComponent(FM_SCRIPT)}` +
-    `?$fm.script.param=${encodeURIComponent(scriptParam)}`;
+  const url = `${base}/fmi/odata/v4/${FM_DB}/${FM_SCRIPT}`;
 
   const fmResp = await fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Authorization":
         "Basic " + Buffer.from(`${FM_USER}:${FM_PASS}`).toString("base64"),
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      scriptParameterValue: scriptParam
+    }),
   });
 
   const fmBody = await fmResp.json();
